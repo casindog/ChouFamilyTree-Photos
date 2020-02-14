4b941e6ad68edbe0c968fedb5122d4556b3c0617
casindog <kevinchou@Admins-MacBook-Pro.local>
2020-02-13 16:41:04 -0800

    reacthooks useContext and useReducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,50 @@
-import React from "react";
-import { Photo } from "./components/photo.component";
-import "./App.css";
+import React, {useReducer, createContext} from "react";
+import Photo from "./components/photo.component";
+
+export const RootContext = createContext()
+
+const initialState = { 
+  file: null,
+  photo: null,
+  album: [],
+  tree: {}
+}
+
+const reducer = (state,action) => {
+  switch (action.type) {
+      case 'FETCH_ALBUM':
+          return {
+              ... state,
+              album: action.payload
+          }
+      case 'SET_FILE':
+          return {
+              ... state,
+              file: action.payload
+          }
+      case 'SET_PHOTO':
+          return {
+              ... state,
+              photo: action.payload
+          }
+
+      default:
+          return state
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
-    <div className="App">
+    <RootContext.Provider value={{state, dispatch}}>
       <h1>Chou Family Tree and Photos</h1>
       <Photo/>
-    </div>
+    </RootContext.Provider>
+
   );
 }
 
 export default App;
+
+
--- frontend/src/components/photo.component.jsx
+++ frontend/src/components/photo.component.jsx
@@ -1,37 +1,34 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useContext, useReducer} from 'react'
 import axios from 'axios';
 import './photo.styles.css'
 import { Tag } from './tag.component'
+import { RootContext } from '../App.js';
 
+// file upload resource
 // https://codeburst.io/image-uploading-using-react-and-node-to-get-the-images-up-c46ec11a7129
-export const Photo = () => {
-    const [file, setFile] = useState(null)
-    const [photo, setPhoto] = useState(null)
-    const [album, setAlbum] = useState(null) 
+
+const Photo = () => {
+    const rootContext = useContext(RootContext) 
 
     useEffect(() => {
-        (async () => {
-            let res = await axios.get('/photos')
-            await setAlbum(res.data)
-        })()
-        // seems like this is still running more than componentdidmount
+        axios.get('/photos')
+            .then(res => { rootContext.dispatch({type: 'FETCH_ALBUM', payload: res.data}) })
+
         // eventually, i want to pass in a selected person from
         // the store to filter the album w/ photos tagged w/ selected person
     },[])
 
-    const handleChange = e => {
-        if (e.target.files.length) {
-            setFile(e.target.files[0])
-        }
+    const handleFile = e => {
+        if (e.target.files.length) { rootContext.dispatch({type: 'SET_FILE', payload: e.target.files[0]}) }
     }
 
     const uploadFile = e => {
         e.preventDefault();
         const fd = new FormData()
-        fd.append('file', file);
+        fd.append('file', rootContext.state.file);
 
         axios.post('./photos', fd)
-            .then(res => { setPhoto(res.data.path) })
+            .then(res => { rootContext.dispatch({type: 'SET_PHOTO', payload: res.data.path}) })
     }   
 
     const tagPersonToImg = e => {
@@ -39,30 +36,30 @@ export const Photo = () => {
         let testid = '5e424127486c82850be92468'
         axios.patch(`./photos/${testid}`, name)
             .then(res => console.log(res))
-        // db.students.update(
-        //     { _id: 1 },
-        //     { $push: { scores: 89 } }
-        //  )
-
     }
 
     const createImages = () => {
-        if (album)  {
+        if (rootContext.state.album)  {
             return (
                 <div id='album'>
-                    {album.map(img => <img key={img._id} onClick={() => setPhoto(img.path)} src={img.path}></img>)}
+                    {rootContext.state.album.map(img => (
+                        <img 
+                            key={img._id} 
+                            onClick={() => rootContext.dispatch({type: 'SET_PHOTO', payload: img.path}) }
+                            src={img.path}>
+                        </img>)
+                    )}
                 </div>
             )
         }
     }
 
-    console.log(album)
     return (
         <div id='photo'>
             <h3>Upload a Photo</h3>
-            <input type='file' onChange={() => handleChange}></input>
-            <button onClick={() => uploadFile}>Upload</button>
-            <img id="main" src={photo}/>
+            <input type='file' onChange={handleFile}></input>
+            <button onClick={uploadFile}>Upload</button>
+            <img id="main" src={rootContext.state.photo}/>
             <Tag/> 
             <button onClick={tagPersonToImg}> Tag Person </button>
 
@@ -71,3 +68,5 @@ export const Photo = () => {
         </div>
     )
 }
+
+export default Photo
